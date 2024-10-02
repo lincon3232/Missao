bb061cd6ecd946f689e27cb3fd15b6f606446bc3
lincon3232 <143628066+lincon3232@users.noreply.github.com>
2024-10-02 19:35:55 -0300

    s

--- script.js
+++ script.js
@@ -20,11 +20,7 @@ const perguntas = [
                     "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia."
                 ]
             }
-        ]
-    },
-    {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [
+        ]        alternativas: [
             {
                 texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: ["Conseguiu utilizar a IA para buscar informações úteis.",
