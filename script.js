a88de0bc846aef735935e7c980aec9577253fcde
lincon3232 <143628066+lincon3232@users.noreply.github.com>
2024-10-02 19:29:08 -0300

    s

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "A que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Lincon pereira",
         alternativas: [
             {
                 texto: "Isso é assustador!",
